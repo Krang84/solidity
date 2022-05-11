7ac28c519c201250b3d05a958fc10e04380e2368
Krang84 <patelassad@gmail.com>
2022-05-11 11:02:45 +0200

    Update test.sol

--- test.sol
+++ test.sol
@@ -3,3 +3,11 @@ pragma solidity >0.6.10 <0.9.0 ;
 contract mycontrat {
     string public variable = "hello solidity";
    }
+
+
+// uint variable 
+
+pragma solidity >0.6.10 <0.9.0 ;
+contract mycontrat {
+    string public variable = "hello solidity";
+   }

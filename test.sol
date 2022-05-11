72ffe5d96920e5922e0acd5f842a4d6a84cd8265
Krang84 <patelassad@gmail.com>
2022-05-11 10:17:58 +0200

    Update test.sol

--- test.sol
+++ test.sol
@@ -1,5 +1,5 @@
 // SPFX-Licence-Identifier : MIT
 pragma solidity >0.6.10 <0.9.0 ;
 contract mycontrat {
-    String public variable = "hello solidity";
+    string public variable = "hello solidity";
    }

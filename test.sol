fb94e1cef5674a66787ab3c203fe48459a04621a
Krang84 <patelassad@gmail.com>
2022-10-27 20:44:19 +0200

    Update test.sol

--- test.sol
+++ test.sol
@@ -1,12 +1,14 @@
-// SPFX-Licence-Identifier : MIT
-
 
 pragma solidity ^0.8.7;
 
 contract test {
 
+     string bitcoin = "nouveau crypto-actif";
 
-
+     struct Person {
+         uint age;
+         string name;
+     }
     
 
 }

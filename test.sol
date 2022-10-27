52018613867ccb75e36ba27ae19032cfe42a9a22
Krang84 <patelassad@gmail.com>
2022-10-27 20:40:04 +0200

    Update test.sol

--- test.sol
+++ test.sol
@@ -1,13 +1,12 @@
 // SPFX-Licence-Identifier : MIT
-pragma solidity >0.6.10 <0.9.0 ;
-contract mycontrat {
-    string public variable = "hello solidity";
-   }
 
 
-// uint variable 
+pragma solidity ^0.8.7;
 
-pragma solidity >0.6.10 <0.9.0 ;
-contract mycontrat {
-    uint compte = 10000000;
-   }
+contract test {
+
+
+
+    
+
+}

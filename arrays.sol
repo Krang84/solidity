4f64aa4179e5f35332aec95dfc7ddd44238379ce
Krang84 <patelassad@gmail.com>
2022-11-03 19:38:34 +0100

    Update arrays.sol

--- arrays.sol
+++ arrays.sol
@@ -1,1 +1,21 @@
 
+pragma solidity ^0.8.7;
+
+contract test {
+
+    
+      // static array with 3 elements
+     uint[3] balance=[10,11,12];
+     
+     // dynamic array
+     uint[] balance2;
+    
+    uint size = 3;
+    uint[] balance3= new uint[](size);
+
+    uint public salary = balance[0];
+    // salary equals to 10
+    uint public valeur = balance[1];
+    // valeur equals to 11
+
+}

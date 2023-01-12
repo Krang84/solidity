b22274f01ad3e39425a8abdfba7d6a21466115fe
Krang84 <patelassad@gmail.com>
2023-01-12 19:43:26 +0100

    Update chap1.sol

--- chap1.sol
+++ chap1.sol
@@ -8,5 +8,14 @@ contract ZombieFactory {
  uint dnaDigits = 16;
  uint dnaModulus = 10 ** dnaDigits;
  
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    // commencez ici
+
+    Zombie[] public zombies;
+ 
  
  }
